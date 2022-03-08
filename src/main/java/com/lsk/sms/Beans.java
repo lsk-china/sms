3a9c5393416d8cce89879539e6772fd66e32bb5f
lsk-china <3075929352@qq.com>
2022-03-08 20:09:38 +0800

    多配置文件

--- src/main/java/com/lsk/sms/Beans.java
+++ src/main/java/com/lsk/sms/Beans.java
@@ -18,7 +18,7 @@ public class Beans {
     public Gson gson() { return new Gson(); }
     @Bean
     public JedisPool jedisPool() {
-        return new JedisPool(new JedisPoolConfig(), "localhost", 6379, 5000, "redisadmin", 3);
+        return new JedisPool(new JedisPoolConfig(), "localhost", 6379, 5000, "lsk123456", 3);
     }
 
 }
